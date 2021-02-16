85dec02e223127733768e98d81e80f4ebd7e7f14
tsuyoshifuchigami <63683392+tsuyoshifuchigami@users.noreply.github.com>
2021-02-16 10:06:46 +0900

    Add files via upload

---
+++ swift/Quiz/Quiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by 渕上豪支 on 2021/02/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
